13c6fa8c6eacb0166d1eef9b80db96316259955d
camilomonsalvem <monsalve88mm@gmail.com>
2025-05-17 18:06:50 -0500

    Add URLs for images in an AWS bucket

--- src/database/Pokeneas.js
+++ src/database/Pokeneas.js
@@ -4,7 +4,7 @@ const Pokeneas = [
         name: "Pikachu",
         height: 0.4,
         ability: "Static",
-        image: "https://storage.googleapis.com/pokeneas-mja-scm/pikachu.png",
+        image: "https://pokeneas-images-taller2.s3.us-east-1.amazonaws.com/pikachu.png",
         quote: "The power of electricity lies not in its strength, but in its versatility"
     },
     {
@@ -12,7 +12,7 @@ const Pokeneas = [
         name: "Charizard",
         height: 1.7,
         ability: "Blaze",
-        image: "https://storage.googleapis.com/pokeneas-mja-scm/charmander.png",
+        image: "https://pokeneas-images-taller2.s3.us-east-1.amazonaws.com/charizard.png",
         quote: "True strength comes from the fire within, not the flames without"
     },
     {
@@ -20,7 +20,7 @@ const Pokeneas = [
         name: "Blastoise",
         height: 1.6,
         ability: "Torrent",
-        image: "https://storage.googleapis.com/pokeneas-mja-scm/blastoise.png",
+        image: "https://pokeneas-images-taller2.s3.us-east-1.amazonaws.com/Blastoise.png",
         quote: "Adaptability is the strongest defense in life's battles"
     },
     {
@@ -28,7 +28,7 @@ const Pokeneas = [
         name: "Venusaur",
         height: 2.0,
         ability: "Overgrow",
-        image: "https://storage.googleapis.com/pokeneas-mja-scm/bulbasaur.png",
+        image: "https://pokeneas-images-taller2.s3.us-east-1.amazonaws.com/Venusaur.png",
         quote: "Growth requires both sunshine and rain"
     },
     {
@@ -36,7 +36,7 @@ const Pokeneas = [
         name: "Gengar",
         height: 1.5,
         ability: "Cursed Body",
-        image: "https://storage.googleapis.com/pokeneas-mja-scm/clefairy.png",
+        image: "https://pokeneas-images-taller2.s3.us-east-1.amazonaws.com/Gengar.png",
         quote: "What others fear in the darkness, I call home"
     },
     {
@@ -44,7 +44,7 @@ const Pokeneas = [
         name: "Gyarados",
         height: 6.5,
         ability: "Intimidate",
-        image: "https://storage.googleapis.com/pokeneas-mja-scm/ponyta.png",
+        image: "https://pokeneas-images-taller2.s3.us-east-1.amazonaws.com/Gyarados.png",
         quote: "Rage can transform you, but wisdom must guide that power"
     },
     {
@@ -52,7 +52,7 @@ const Pokeneas = [
         name: "Snorlax",
         height: 2.1,
         ability: "Immunity",
-        image: "https://storage.googleapis.com/pokeneas-mja-scm/golem.png",
+        image: "https://pokeneas-images-taller2.s3.us-east-1.amazonaws.com/Snorlax.png",
         quote: "Rest is not idleness; it is the foundation of all great action"
     },
     {
@@ -60,7 +60,7 @@ const Pokeneas = [
         name: "Mewtwo",
         height: 2.0,
         ability: "Pressure",
-        image: "https://storage.googleapis.com/pokeneas-mja-scm/pidgey.png",
+        image: "https://pokeneas-images-taller2.s3.us-east-1.amazonaws.com/Mewtwo.png",
         quote: "The circumstances of one's birth are irrelevant; it is what you do with the gift of life"
     }
 ]
