b12bb4592efdda2bcd2146a5ba0447774c246082
camilomonsalvem <monsalve88mm@gmail.com>
2025-05-17 16:31:08 -0500

    First commit

---
+++ src/database/Pokeneas.js
@@ -0,0 +1,15 @@
+const Pokeneas = [
+    {
+    id: 1,
+    name: "Ibeesaur",
+    height: 1.25,
+    skills: "Powerful sting",
+    image: " ",
+    quote:
+        "With my trusty honeycomb stick, I command an army of bees to do my bidding. So fear!",
+    }
+];
+
+module.exports = {
+    Pokeneas,
+};
